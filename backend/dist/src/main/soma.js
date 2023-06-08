02a08c6fbc9669d2a4581af7841cc988d30950c1
Cleberbrant <cleberobb@hotmail.com>
2023-06-08 11:19:00 -0300

    initial commit

---
+++ backend/dist/src/main/soma.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.soma = void 0;
+function soma(a, b) {
+    return a + b;
+}
+exports.soma = soma;
+//# sourceMappingURL=soma.js.map
